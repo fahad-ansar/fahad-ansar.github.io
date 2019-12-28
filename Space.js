9f52c90f72aec6b338d2ea288792dc8f4f811b45
fa16np <fa16np@brocku.ca>
2019-12-28 04:46:44 -0500

    Flowchart added

--- Space.js
+++ Space.js
@@ -42,16 +42,21 @@ function addrecta(){
 
     stage.add(layer);
 
-    box.on('mouseover', function() {
+    layer.on('mouseover', function() {
       document.body.style.cursor = 'pointer';
     });
-    box.on('mouseout', function() {
+
+    layer.on('dblclick', function(){
+      layer.destroy();
+    });
+
+    layer.on('mouseout', function() {
       document.body.style.cursor = 'default   ';
     });
    }
 
 
-//-----------------------------------------------------------------------
+//-------------------------------Pointerstraight--------------------------------
 
 
 function addPointV(){
@@ -61,7 +66,7 @@ var layer = new Konva.Layer();
 
 
 var line = new Konva.Line({
-    points: [50, 50, 50, 80+size],
+    points: [100, 80, 100, 80+size/2],
     stroke: 'black',
     strokeWidth: 3,
     lineCap: 'round',
@@ -74,10 +79,96 @@ layer.add(line);
 
 stage.add(layer);
 
-box.on('mouseover', function() {
+layer.on('mouseover', function() {
     document.body.style.cursor = 'pointer';
 });
-box.on('mouseout', function() {
+layer.on('mouseout', function() {
     document.body.style.cursor = 'default   ';
 });
 }
+
+
+//-----------------------------------Start/End-----------------------------
+
+
+function addStart(text, clr){
+
+
+var layer = new Konva.Layer();
+
+var group = new Konva.Group({
+    draggable: true
+  });
+
+var oval = new Konva.Ellipse({
+    x: stage.width() / 2,
+    y: 100,
+    radiusX: 50,
+    radiusY: 25,
+    fill: clr,
+    stroke: 'black',
+    strokeWidth: 2
+  });
+
+var textp = new Konva.Text({
+    x: 920,
+    y: 75,
+    text: text,
+    fontSize: 20,
+    fontFamily: 'Calibri',
+    fill: '#black',
+    padding: 18,
+    align: 'center'       
+});
+
+group.add(oval);
+group.add(textp);
+    
+layer.add(group);
+
+stage.add(layer); 
+
+
+layer.on('mouseover', function() {
+    document.body.style.cursor = 'pointer';
+});
+layer.on('mouseout', function() {
+    document.body.style.cursor = 'default   ';
+});
+}
+
+
+//-----------------------------------Connector-----------------------------
+
+
+function addCnct(){
+    
+    
+    var layer = new Konva.Layer();
+    
+    
+    var circle = new Konva.Circle({
+        x: stage.width() / 2,
+        y: stage.height() / 2,
+        radius: 15,
+        fill: 'darkgrey',
+        stroke: 'black',
+        strokeWidth:1,  
+        draggable: true
+    });
+    
+        
+    layer.add(circle);
+    
+    stage.add(layer);
+    
+    line.on('mouseover', function() {
+        document.body.style.cursor = 'pointer';
+    });
+
+    
+
+    line.on('mouseout', function() {
+        document.body.style.cursor = 'default   ';
+    });
+    }
