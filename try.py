8f08c31f99a14f825a40d7351137fb88985ea418
fa16np <fa16np@brocku.ca>
2020-04-13 01:26:15 -0400

    Corona update trial

--- try.py
+++ try.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python
 
-print "Content-type:text/html\r\n\r\n"
-print '<html>'
-print '<head>'
-print '<title>Hello World - First CGI Program</title>'
-print '</head>'
-print '<body>'
-print '<h2>Hello World! This is my first CGI program</h2>'
-print "213"
-print '</body>'
-print '</html>'
+import random
+
+print "Content-Type: text/html"
+print
+print "hi"*random.randrange(4,10)
