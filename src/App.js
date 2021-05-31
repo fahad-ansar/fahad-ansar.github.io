177b504ce09fed2aaae46d9c2d3d3f0474fdaa14
fahad-ansar <fa16np@brocku.ca>
2021-05-31 19:46:18 -0400

    exp.

--- src/App.js
+++ src/App.js
@@ -1,71 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import logo from './logo.svg';
 import './App.css';
-import Rosh from './Games/Rosh';
-// import RighBar from './Sidebar/rightbar';
-import '@trendmicro/react-sidenav/dist/react-sidenav.css';
-import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
 
-import { Route, Switch ,  BrowserRouter as Router} from 'react-router-dom';
-
-import about from './Pages/about';
-// import navBar from './Sidebar/tempNav';
-
-
-
-class App extends React.Component {
-
-  render(){
-    return (
-      <>
-      {/* <navBar/> */}
-      <Router>
-            <Route render={({ location, history }) => (
-                <React.Fragment>
-                    <SideNav
-                        onSelect={(selected) => {
-                            const to = '/' + selected;
-                            if (location.pathname !== to) {
-                                history.push(to);
-                            }
-                        }}
-                    >
-                        <SideNav.Toggle />
-                        <SideNav.Nav defaultSelected="home">
-                            <NavItem eventKey="home">
-                                <NavIcon>
-                                    <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
-                                </NavIcon>
-                                <NavText>
-                                    Home
-                                </NavText>
-                            </NavItem>
-                            <NavItem eventKey="devices">
-                                <NavIcon>
-                                    <i className="fa fa-fw fa-device" style={{ fontSize: '1.75em' }} />
-                                </NavIcon>
-                                <NavText>
-                                    Devices
-                                </NavText>
-                            </NavItem>
-                        </SideNav.Nav>
-                    </SideNav>
-                    <main>
-                        <Route path="/home" component={props => <about />} />
-                    </main>
-                </React.Fragment>
-            )}
-            />
-         </Router>
-      <h1 style={{alignContent:"flex-start" , textAlign:"left" }}>sdfdsf</h1>
-
-      <div className="Ros">
-      <Rosh />
-      </div>
-      </>
-     );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
-
 
 export default App;
--- src/Games/Player.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import  r from './weapons/r.png'
-import  pr from './weapons/pr.png'
-import  s from './weapons/s.png'
-import  p from './weapons/p.png'
-
-
-
-export default Player;
